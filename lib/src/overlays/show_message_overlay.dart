1e7dbe955fddd048901693ed29f8453bbb5cc76f
robmllze <86869208+robmllze@users.noreply.github.com>
2024-10-10 01:52:19 +1100

    Update

--- lib/src/overlays/show_message_overlay.dart
+++ lib/src/overlays/show_message_overlay.dart
@@ -25,6 +25,7 @@ Future<void> showMessageOverlay(
   Widget? trailing,
   bool tapBackgroundToDismiss = false,
   EdgeInsets? padding,
+  EdgeInsets? margin,
   double? spacing,
   double? titleIconSpacing,
   BoxDecoration? decoration,
@@ -39,48 +40,51 @@ Future<void> showMessageOverlay(
     tapBackgroundToDismiss: tapBackgroundToDismiss,
     builder: (context, remove) {
       remover?.call(remove);
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          IntrinsicWidth(
-            child: ClippedSurface(
-              padding: padding ?? EdgeInsets.all(32.sc),
-              decoration: decoration,
-              color: color,
-              borderRadius: borderRadius,
-              width: width,
-              height: height,
-              child: DividedColumn(
-                divider: SizedBox(height: spacing ?? 16.sc),
-                children: [
-                  if (title != null) ...[
-                    Wrap(
-                      children: [
-                        Text(
-                          title,
-                          style: titleStyle,
-                        ),
-                        if (titleIcon != null) ...[
-                          SizedBox(width: titleIconSpacing ?? 8.sc),
-                          titleIcon,
+      return Padding(
+        padding: margin ?? EdgeInsets.all(32.sc),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IntrinsicWidth(
+              child: ClippedSurface(
+                padding: padding ?? EdgeInsets.all(32.sc),
+                decoration: decoration,
+                color: color,
+                borderRadius: borderRadius,
+                width: width,
+                height: height,
+                child: DividedColumn(
+                  divider: SizedBox(height: spacing ?? 16.sc),
+                  children: [
+                    if (title != null) ...[
+                      Wrap(
+                        children: [
+                          Text(
+                            title,
+                            style: titleStyle,
+                          ),
+                          if (titleIcon != null) ...[
+                            SizedBox(width: titleIconSpacing ?? 8.sc),
+                            titleIcon,
+                          ],
                         ],
-                      ],
-                    ),
-                    const ContentDivider(),
+                      ),
+                      const ContentDivider(),
+                    ],
+                    if (leading != null) leading,
+                    if (message != null)
+                      Text(
+                        message.toString(),
+                        style: messageStyle,
+                      ),
+                    if (trailing != null) trailing,
                   ],
-                  if (leading != null) leading,
-                  if (message != null)
-                    Text(
-                      message.toString(),
-                      style: messageStyle,
-                    ),
-                  if (trailing != null) trailing,
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     },
   );
